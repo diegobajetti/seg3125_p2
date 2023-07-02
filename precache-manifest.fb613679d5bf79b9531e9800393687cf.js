834880a8dce18aaecb29e2be80b2340b99017acb
diegobajetti <diego.bajetti@outlook.com>
2023-07-02 19:01:20 -0400

    Updates

---
+++ precache-manifest.fb613679d5bf79b9531e9800393687cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d50bca165cb218dcee2",
+    "url": "/seg3125_p2.github.io/static/css/main.73939e14.chunk.css"
+  },
+  {
+    "revision": "8d50bca165cb218dcee2",
+    "url": "/seg3125_p2.github.io/static/js/main.1ed9c8b8.chunk.js"
+  },
+  {
+    "revision": "818a0e07437b2fc832b6",
+    "url": "/seg3125_p2.github.io/static/js/runtime~main.0b1c3cc5.js"
+  },
+  {
+    "revision": "41e567c827714291b93f",
+    "url": "/seg3125_p2.github.io/static/js/2.a676d882.chunk.js"
+  },
+  {
+    "revision": "2126394ed55a63dd18152f10ed881076",
+    "url": "/seg3125_p2.github.io/index.html"
+  }
+];
