5ec00809128aef2b1af11c5c7461bd7427e46b32
diegobajetti <diego.bajetti@outlook.com>
2023-07-03 00:27:53 -0400

    Updates

---
+++ precache-manifest.156b4a59fc9d98af03fb58cd55c2b647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ae09673e437aae12599",
+    "url": "/seg3125_p2.github.io/static/css/main.b768ed70.chunk.css"
+  },
+  {
+    "revision": "0ae09673e437aae12599",
+    "url": "/seg3125_p2.github.io/static/js/main.5a6d44cf.chunk.js"
+  },
+  {
+    "revision": "818a0e07437b2fc832b6",
+    "url": "/seg3125_p2.github.io/static/js/runtime~main.0b1c3cc5.js"
+  },
+  {
+    "revision": "79a8c7264e1ef4df4e5e",
+    "url": "/seg3125_p2.github.io/static/js/2.8349cdf2.chunk.js"
+  },
+  {
+    "revision": "c6ab6a8aa615dd4c9f794280ff3051e2",
+    "url": "/seg3125_p2.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83ab776fc3eced639c0bac5c8fc37639.js"
+  "/seg3125_p2.github.io/precache-manifest.156b4a59fc9d98af03fb58cd55c2b647.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/seg3125_p2.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
